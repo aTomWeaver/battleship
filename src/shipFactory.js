e2db8793fd1e8dede1b2023577efbfc0edf3c1db
atomweaver <atomweavermail@gmail.com>
2022-07-12 17:08:59 -0400

    add ship types

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -1,12 +1,30 @@
-function shipFactory(position) {
-  const len = position.length; // currently unused
+function getShipLength(type) {
+  switch (type) {
+    case "carrier":
+      return 5;
+    case "battleship":
+      return 4;
+    case "cruiser":
+      return 3;
+    case "submarine":
+      return 3;
+    case "destroyer":
+      return 2;
+  }
+}
+
+function shipFactory(type, position) {
+  const length = getShipLength(type); // currently unused
   let hits = [];
   const isSunk = () => position.every((space) => hits.includes(space));
-  const hit = (num) => {
-    if (position.includes(num)) hits.push(num);
-    return true;
+  const tryHit = (num) => {
+    if (position.includes(num) && !hits.includes(num)) {
+      hits.push(num);
+      return true;
+    }
+    return false;
   };
-  return { isSunk, hit, hits };
+  return { isSunk, tryHit, hits, length };
 }
 
 export { shipFactory };
--- src/tests/shipFactory.test.js
+++ src/tests/shipFactory.test.js
@@ -1,30 +1,50 @@
 import {shipFactory} from '../shipFactory.js';
 
+test('ship type sets correct length', () => {
+    const carrier = shipFactory('carrier', [1, 2, 3, 4, 5]);
+    expect(carrier.length).toBe(5);
+    const destroyer = shipFactory('destroyer', [6, 7]);
+    expect(destroyer.length).toBe(2);
+    const cruiser = shipFactory('cruiser', [1, 2, 3]);
+    expect(cruiser.length).toBe(3);
+    const submarine = shipFactory('submarine', [1, 2, 3]);
+    expect(submarine.length).toBe(3);
+    const battleship = shipFactory('battleship', [1, 2, 3, 4]);
+    expect(battleship.length).toBe(4);
+})
 
-test('ship objects take hits with hit function', () => {
-    const ship = shipFactory([1, 2, 3]);
-    const ship2 = shipFactory([6, 7, 8, 9]);
-    ship.hit(1);
-    ship.hit(2);
-    ship2.hit(4);
+test('ship objects updates hits with successful tryHit call', () => {
+    const ship = shipFactory('submarine',[1, 2, 3]);
+    const ship2 = shipFactory('cruiser', [6, 7, 8, 9]);
+    ship.tryHit(1);
+    ship.tryHit(2);
+    ship2.tryHit(4);
     // expect(ship.isSunk).toBe(false);
     expect(ship.hits).toStrictEqual([1, 2]);
     expect(ship2.hits).toStrictEqual([]);
     expect(ship.hits).not.toStrictEqual([1, 2, 3])
 });
 
-test('ships return true when hit', () => {
-    const ship = shipFactory([1, 2, 3]);
-    expect(ship.hit()).toBe(true);
+test('tryHit returns true when hit and false when not hit', () => {
+    const ship = shipFactory('submarine', [1, 2, 3]);
+    expect(ship.tryHit(2)).toBe(true);
+    expect(ship.tryHit(4)).toBe(false);
+    expect(ship.tryHit(0)).toBe(false);
 })
 
+test('tryHit returns false if it has already been hit', () => {
+    const ship = shipFactory('destoyer', [1, 2])
+    expect(ship.tryHit(1)).toBe(true);
+    expect(ship.tryHit(1)).toBe(false);
+} )
+
 test('ship sinks when all positions are hit', () => {
-    const ship = shipFactory([8, 9, 10]);
-    ship.hit(10);
-    ship.hit(8);
-    ship.hit(7);
+    const ship = shipFactory('submarine', [8, 9, 10]);
+    ship.tryHit(10);
+    ship.tryHit(8);
+    ship.tryHit(7);
     expect(ship.isSunk()).not.toBe(true);
-    ship.hit(9);
+    ship.tryHit(9);
     expect(ship.isSunk()).toBe(true);
 })
 
