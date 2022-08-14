4d595d2084a410472bf918d0080536dcb99286fa
aTomWeaver <atomweavermail@gmail.com>
2022-08-14 19:34:21 -0400

    add main page render function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { game } from "./gameLoop.js";
-import 'render.js';
+import { renderMain } from "./render.js";
 
 const GAME = new game();
 const p1 = GAME.p1;
@@ -7,15 +7,15 @@ const cpu = GAME.cpu;
 
 GAME.defaultSetup();
 
-const attackBtn = document.getElementById('test-button');
-attackBtn.addEventListener('click', () => {
-  // check to see if player's choice was valid before proceeding
-  if(GAME.makeMove(p1, 2, cpu)) {
-    console.log(`cpu hits: ${cpu.board.totalHits}`);
-    GAME.update();
-    GAME.makeMove(cpu, GAME.getCpuMove(), p1);
-    console.log(`p1 hits: ${p1.board.totalHits}`);
-    GAME.update();
-  };
-})
+// const attackBtn = document.getElementById('test-button');
+// attackBtn.addEventListener('click', () => {
+//   // check to see if player's choice was valid before proceeding
+//   if(GAME.makeMove(p1, 2, cpu)) {
+//     console.log(`cpu hits: ${cpu.board.totalHits}`);
+//     GAME.update();
+//     GAME.makeMove(cpu, GAME.getCpuMove(), p1);
+//     console.log(`p1 hits: ${p1.board.totalHits}`);
+//     GAME.update();
+//   };
+// })
 
--- src/render.js
+++ src/render.js
@@ -1,1 +1,50 @@
+function renderMain() { 
+  const pageCtr = document.querySelector('.page-ctr');
+  
+  const pageTitle = document.createElement('h1');
+  pageTitle.id = 'page-title';
 
+  const appCtr = document.createElement('div');
+  appCtr.classList.add('app-ctr');
+
+  // CPU ELEMENTS
+  const cpuActionDisplay = document.createElement('div');
+  cpuActionDisplay.classList.add('cpu-action-display');
+
+  const cpuBoard = document.createElement('div');
+  cpuBoard.classList.add('board');
+
+  const cpuHUD = document.createElement('div');
+  cpuHUD.classList.add('cpu-hud hud');
+
+  const cpuTitle = document.createElement('h2');
+  cpuTitle.classList.add('player-hud-title');
+
+  const cpuSunkenShips = document.createElement('div');
+  cpuSunkenShips.classList.add('sunken-ships');
+
+  // P1 ELEMENTS
+
+  const p1HUD = document.createElement('div');
+  p1HUD.classList.add('p1-hud hud');
+
+  const p1Title = document.createElement('h2');
+  p1Title.classList.add('player-hud-title');
+
+  const p1SunkenShips = document.createElement('div');
+  p1SunkenShips.classList.add('sunken-ships');
+
+  const p1Board = document.createElement('div');
+  p1Board.classList.add('board');
+
+  const p1ActionDisplay = document.createElement('div');
+  p1ActionDisplay.classList.add('p1-action-display');
+
+  cpuHUD.append(cpuTitle, cpuSunkenShips);
+  p1HUD.append(p1Title, p1SunkenShips);
+  appCtr.append(cpuActionDisplay, cpuBoard, cpuHUD, p1HUD, p1Board, p1ActionDisplay);
+  pageCtr.append(pageTitle, appCtr);
+  document.body.append(pageCtr);
+}
+
+export {renderMain};
