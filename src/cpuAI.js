50cce9500b1bac9761e577c0a1d3210580bac5b1
aTomWeaver <atomweavermail@gmail.com>
2022-10-31 18:42:38 -0400

    target ship mvp for one hit or two hits

--- src/cpuAI.js
+++ src/cpuAI.js
@@ -1,8 +1,9 @@
 const coinflip = () => Math.floor(Math.random() * 2);
 
 class targetShip {
-  constructor(...hits) {
+  constructor(attackedSpaces, ...hits) {
     this.spaces = [...hits];
+    this.unavailableSpaces = [...attackedSpaces]
   }
   get orientation() {
     console.log(this.spaces.length)
@@ -16,52 +17,47 @@ class targetShip {
     }
   }
   getTarget() {
+    let target;
     if (this.orientation == 'none') {
-      this.#attackPerimeter(this.spaces[0]);
+      target = this.#attackPerimeter(this.spaces[0]);
     } else {
       let lowestValHit = Math.min(...this.spaces);
       let highestValHit = Math.max(...this.spaces);
       console.log(lowestValHit, highestValHit);
       if (this.orientation == "vertical") {
         if (this.#isValidTarget(highestValHit + 10)) {
-          return highestValHit + 10;
+          target = highestValHit + 10;
         } else if (this.#isValidTarget(lowestValHit - 10)) {
-          return lowestValHit - 10;
+          target = lowestValHit - 10;
         } else {
           console.log('no valid move');
         }
       } else {
         if (this.#isValidTarget(highestValHit + 1)) {
-          return highestValHit + 1;
+          target = highestValHit + 1;
         } else if (this.#isValidTarget(lowestValHit - 1)) {
-          return lowestValHit - 1;
-        } else {
-          console.log('no valid move');
+          target = lowestValHit - 1;
         }
       }
     }
+    this.unavailableSpaces.push(target);
+    return target;
   }
   #isValidTarget(target) { 
-    // I don't like the below solution but it's workable
-    const hitArray = [...document.querySelectorAll('.hit')].map(elm => parseInt(elm.dataset.index));
-    const missArray = [...document.querySelectorAll('.miss')].map(elm => parseInt(elm.dataset.index));
-    const totalAttemptsArray = [...missArray,...hitArray];
-
-    if (!totalAttemptsArray.includes(target)) {
-      if (this.orientation == "horizontal") {
+    if (!this.unavailableSpaces.includes(target)) {
+      let center = this.spaces[0];
+      if (this.orientation == "horizontal" || (target == center - 1 || target == center + 1)) {
         if (Math.floor(this.spaces[0] * 0.1) == Math.floor(target * 0.1)) {
           return true;
         } else {
           return false;
         }
-      } else if (this.orientation == "vertical") {
+      } else if (this.orientation == "vertical" || (target == center - 10 || target == center + 10)) {
         if (target > 0 && target < 100) {
           return true;
         } else {
           return false;
         }
-      } else {
-
       }
     } else {
       return false;
@@ -73,6 +69,7 @@ class targetShip {
     const right = center + 1;
     const down = center + 10;
     const directions = [left, up, right, down];
+    
     return directions.find(dir => this.#isValidTarget(dir))
   }
 }
--- src/tests/cpuAI.test.js
+++ src/tests/cpuAI.test.js
@@ -1,8 +1,21 @@
 import {targetShip} from '../cpuAI.js'
 
 test("generates a valid targetShip", () => {
-    const ship = new targetShip(0, 1);
-    expect(ship.orientation).toBe('horizontal');
-    expect(ship.spaces).toStrictEqual([0, 1]);
+    const ship = new targetShip([20, 42, 63], 20);
+    expect(ship.orientation).toBe('none');
+    expect(ship.spaces).toStrictEqual([20]);
+    expect(ship.unavailableSpaces)
+        .toStrictEqual([20, 42, 63])
 });
 
+ test("tests validity of targets", () => {
+    // ship with only one hit
+    const ship = new targetShip([80, 90], 90);
+    expect(ship.getTarget()).toBe(91)
+    // horizontal ship
+    const ship2 = new targetShip([35, 45, 55], 45, 46);
+    expect(ship2.getTarget()).toBe(47);
+    // vertical ship
+    const ship3 = new targetShip([19, 29, 39], 19, 29);
+    expect(ship3.getTarget()).toBe(9)
+});
